3d7c57dad882fc3ccf02e9601e823f5f13d0396d
Nymph <82485126+RhizoNymph@users.noreply.github.com>
2024-08-19 00:04:19 -0700

    Create base-dev.nix

---
+++ base-dev.nix
@@ -0,0 +1,28 @@
+{
+  description = "Base development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            git
+            curl
+            wget
+            gnumake
+            gcc
+          ];
+
+          shellHook = ''
+          '';
+        };
+      });
+}
